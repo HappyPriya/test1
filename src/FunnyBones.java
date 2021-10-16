f075cd8c5d8709f166dddde761ad4b8581954731
HappyPriya <priya042613@gmail.com>
2021-10-16 16:42:06 +0100

    first commit

---
+++ src/FunnyBones.java
@@ -0,0 +1,13 @@
+public class FunnyBones{
+    public static void main(String[] args)
+    {
+        System.out.println("Funny Bones");
+        System.out.println("Age 18");
+        System.out.println("Salary £9999");
+        System.out.println("Percentage 100%");
+        System.out.println("Address Happy Lane HA1 1PY");
+
+    }
+}
+
+
